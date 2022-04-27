ffe9f7339d1b6d449676ed34582feb827a4b66c1
arharros <rtulumbaev@gmail.com>
2022-04-27 19:12:58 +0500

    V1.02

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,62 +1,39 @@
 
-import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 
 public class MainTest {
-
-
     public static void main (String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","E:\\JavaProject\\Tools\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://demoqa.com/");
+        WebDriver browser = new ChromeDriver();
+        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        browser.get("https://demoqa.com/");
         Thread.sleep(3000);
-        WebElement element = driver.findElement(By.xpath("(//div[@class='card mt-4 top-card'])[1]"));
-        element.click();
+        WebElement elementCard = browser.findElement(By.xpath("(//div[@class='card mt-4 top-card'])[1]"));
+        elementCard.click();
 
-        //Открываем Text Box
-        element = driver.findElement(By.xpath("//span[contains(text(),'Text Box')]"));
-        WebElement parentElement = element.findElement(By.xpath("//span[contains(text(),'Text Box')]/parent::li"));
-        String classOfElement = parentElement.getAttribute("class");
+        WebElement textBox = browser.findElement(By.xpath("//span[contains(text(),'Text Box')]"));
+        WebElement parentTextBox = textBox.findElement(By.xpath("//span[contains(text(),'Text Box')]/parent::li"));
+        String classOfElement = parentTextBox.getAttribute("class").toLowerCase();
         Thread.sleep(3000);
-        if (!classOfElement.equals("btn btn-light active")) element.click();
+        if (!classOfElement.contains("active")) textBox.click();
 
-        /*
-        //Первый вариант заполнения
-        element = driver.findElement(By.xpath("//input[@id='userName']"));
-        element.clear();
-        element.sendKeys("Иванов Иван Иванович");
-
-        element = driver.findElement(By.xpath("//input[@id='userEmail']"));
-        element.clear();
-        element.sendKeys("ivanov@gmail.com");
-
-        element = driver.findElement(By.xpath("//textarea[@id='currentAddress']"));
-        element.clear();
-        element.sendKeys("453118 г.Стерлитамак, ул.Курчатова д.6 кв.56");
-
-        element = driver.findElement(By.xpath("//textarea[@id='permanentAddress']"));
-        element.clear();
-        element.sendKeys("453118 г.Стерлитамак, ул.Сакко и Ванцетти д.3 кв.10");
-*/
-        //Второй вариант заполнения через создание метода
-        findTxtElementAndFill(driver, "//input[@id='userName']", "Иванов Иван Иванович");
-        findTxtElementAndFill(driver, "//input[@id='userEmail']", "ivanov@gmail.com");
-        findTxtElementAndFill(driver, "//textarea[@id='currentAddress']", "453118 г.Стерлитамак, ул.Курчатова д.6 кв.56");
-        findTxtElementAndFill(driver, "//textarea[@id='permanentAddress']", "453118 г.Стерлитамак, ул.Сакко и Ванцетти д.3 кв.10");
-
+        findTxtBoxAndFill(browser, "//input[@id='userName']", "Иванов Иван Иванович");
+        findTxtBoxAndFill(browser, "//input[@id='userEmail']", "ivanov@gmail.com");
+        findTxtBoxAndFill(browser, "//textarea[@id='currentAddress']", "453118 г.Стерлитамак, ул.Курчатова д.6 кв.56");
+        findTxtBoxAndFill(browser, "//textarea[@id='permanentAddress']", "453118 г.Стерлитамак, ул.Сакко и Ванцетти д.3 кв.10");
+        WebElement submit = browser.findElement(By.id("submit"));
+        submit.click();
         Thread.sleep(5000);
-        driver.quit();
+        browser.quit();
     }
 
-    public static void findTxtElementAndFill (WebDriver driver, String elementXpath, String textFill) {
-        WebElement element = driver.findElement(By.xpath(elementXpath));
-        element.clear();
-        element.sendKeys(textFill);
+    public static void findTxtBoxAndFill(WebDriver browser, String elementXpath, String textFill) {
+        WebElement textBox = browser.findElement(By.xpath(elementXpath));
+        textBox.clear();
+        textBox.sendKeys(textFill);
     }
 }
